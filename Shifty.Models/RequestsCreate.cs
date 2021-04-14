6573749b85b650f3c94e2b3e14ebfe8557e7f7f5
Justin plaschka <jtplasch@gmail.com>
2021-04-14 12:05:32 -0400

    Create methods

---
+++ Shifty.Models/RequestsCreate.cs
@@ -0,0 +1,20 @@
+﻿using ShiftyApp.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shifty.Models
+{
+    public class RequestsCreate
+    {
+        [Required]
+        public RequestType TypeOfRequest { get; set; }
+        public string Reason { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        public int EmployeeId { get; set; }
+    }
+}
---
+++ Shifty.Models/WorkScheduleCreate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shifty.Models
+{
+    public class WorkScheduleCreate
+    {
+        [Required]
+        public DateTime Date { get; set; }
+        public int EmployeeId { get; set; }
+    }      
+}
